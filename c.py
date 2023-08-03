dd69895934d0ac98d2803f6309ccfa7663d166c1
kelvinfu123 <137789236+kelvinfu123@users.noreply.github.com>
2023-08-04 04:00:37 +0930

    Add files via upload

---
+++ c.py
@@ -0,0 +1,3 @@
+# OpenWeatherMap API Key
+weather_api_key = "f5b2dd7655dd5646fb3641cc9487dc60"
+url = "http://api.openweathermap.org/data/2.5/weather?units=Imperial&APPID=f5b2dd7655dd5646fb3641cc9487dc60"
